54f007dd733bd273d54530cb8ddf97a66af9b3bc
Stewart French <stewartfrench@me.com>
2015-11-25 18:49:05 +0000

    Set up Devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :products
+  devise_for :users
+  root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
