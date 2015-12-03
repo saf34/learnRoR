b71146a43e22ca56fe41c9f9d1975049783b1cb7
Stewart French <stewartfrench@me.com>
2015-12-03 19:13:02 +0000

    Seed default user, remove sign up

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.delete_all
+# Seed some default users in Devise
+User.delete_all
+user = User.new
+user.email = 'stewartfrench@me.com'
+user.password = 'kitijooz'
+user.password_confirmation = 'kitijooz'
+user.save!
+
 # Seed Videos with the core B.A.S.I.S. course videos
-Video.create!(title: '1: Balance', description: %{<p>The first section of our technical BASIS is Balance.  In this video you will learn the three essential balance stances that are the foundation of your technique.  You need to practise holding each for a few minutes each day, with mindful focus on a small collection of focal points.</p>}, icon_url: 'icons/balance.png', banner_url: 'banners/balance.jpg', thumbnail_url: 'thumbnails/balance.jpg', vimeo_id: '147609629')
+Video.delete_all
+Video.create!(title: '1: Balance', description: %{The first section of our technical BASIS is Balance.  In this video you will learn the three essential balance stances that are the foundation of your technique.  You need to practise holding each for a few minutes each day, with mindful focus on a small collection of focal points.}, icon_url: 'icons/balance.png', banner_url: 'banners/balance.jpg', thumbnail_url: 'thumbnails/balance.jpg', vimeo_id: '147609629')
+
 
