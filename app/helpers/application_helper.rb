cc900a51c6924579fcceb78f3a783b8c8b8b2b30
Stewart French <stewartfrench@me.com>
2015-12-03 04:02:27 +0000

    Style login and sign up.  Set up Dashboard app template roughly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  # Change to use object properties e.g. url(#{@user.background_image})
+  def show_user_bg
+    "background-image: url(#{asset_path("iceland.jpg")});"
+  end
 end
